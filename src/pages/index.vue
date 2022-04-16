e19d4a6a1b2b8069e43ef1030e0cc9c37f1d2144
MS-XUWBIYXDNAOI\Administrator <97039021@qq.com>
2022-04-16 13:21:09 +0800

    增长  贪吃蛇

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,36 +1,79 @@
 <template>
-    <span class="gameBtn" :class="{'active':getActive(game.name)}" @click="routerPush(game.path)" v-for="game in gameList" :key="game.name">{{game.name}} </span>
+    <div class="gameBtn">
+        <div class="menuItem" :class="{ 'active': getActive(game.name) }" v-for="game in gameList" :key="game.name"
+            @click="routerPush(game.path)">
+            <div class="iconList">
+                <component class="iconItem" :is="game.iconComp"></component>
+            </div>
+            <span>{{ game.name }} </span>
+        </div>
+    </div>
     <router-view></router-view>
 </template>
 
 <script setup lang="ts">
-    import { ref } from 'vue';
-    import {games} from '../router/index'
-    import {useRoute, useRouter, RouteRecord} from 'vue-router'
-    let router = useRouter();
-    let gameList = ref([...games])
-    function getActive(name: string){
-        if(router.currentRoute.value.name === name){
-            return true;
-        }
-        return false
+import { ref } from 'vue';
+import { createFromIconfontCN } from '@ant-design/icons-vue';
+import { games } from '../router/index'
+import { useRoute, useRouter, RouteRecord } from 'vue-router'
+
+let router = useRouter();
+let gameList = ref([...games])
+function getActive(name: string) {
+    if (router.currentRoute.value.name === name) {
+        return true;
     }
-    
-    function routerPush(path: string){
-        router.push(path);
-    }
+    return false
+}
+
+function routerPush(path: string) {
+    router.push(path);
+}
 
 </script>
 
 <style lang="less" scoped>
-    .gameBtn{
-        cursor: pointer;
-        margin: 0 10px;
-        &:hover{
-            color:aquamarine;
-        }
-        &.active{
-            color:aqua;
+.gameBtn {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    cursor: pointer;
+    margin: 0 10px;
+    flex-wrap: wrap;
+
+    .menuItem {
+        padding: 5px 10px;
+
+        .iconList {
+            width: 40px;
+            height: 40px;
+            padding: 5px;
+            box-shadow: 0 0 2px 0px #cbcbcb;
+
+            .iconItem {
+                width: 100%;
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
         }
     }
+
+    >div:hover {
+        color: #0dd1d7;
+
+        .iconList {
+            box-shadow: 0 0 2px 0px #0dd1d7;
+        }
+    }
+
+    >div.active {
+        color: #0d91d7;
+
+        .iconList {
+            box-shadow: 0 0 2px 0px #0d91d7;
+        }
+    }
+}
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,51 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from "vue-router";
 
+const games = [
+    {
+        path: "/gameList/minesweeper",
+        name: "扫雷",
+        icon: "MineSpweer",
+        iconComp: null,
+        component: () => import("../pages/mineSpweer/minesweeper.vue"),
+    },
+    {
+        path: "/gameList/gobang",
+        name: "五子棋",
+        icon: "Gobang",
+        iconComp: null,
+        component: () => import("../pages/gobang/index.vue"),
+    },
+    {
+        path: "/gameList/gluttonous",
+        name: "贪吃蛇",
+        icon: "Gluttonous",
+        iconComp: null,
+        component: () => import("../pages/gluttonous/index.vue"),
+    },
+];
+games.forEach(async (item) => {
+    let iconComp = await import(`../assets/icons/${item.icon}.svg?component`);
+    item.iconComp = iconComp.default;
+});
 
-const games = [{
-    path:"/gameList/minesweeper",
-    name:"扫雷",
-    component: () => import("../pages/mineSpweer/minesweeper.vue")
-},{
-    path:"/gameList/gobang",
-    name:"五子棋",
-    component: () => import("../pages/gobang/index.vue")
-}]
-
-const routes = [{
-    path:"/",
-    name:"/",
-    redirect:"/gameList"
-},{
-    path: "/gameList",
-    name: "gameList",
-    component: () => import("../pages/index.vue"),
-    children: [...games]
-}]
+const routes = [
+    {
+        path: "/",
+        name: "/",
+        redirect: "/gameList",
+    },
+    {
+        path: "/gameList",
+        name: "gameList",
+        component: () => import("../pages/index.vue"),
+        children: [...games],
+    },
+];
 
 const router = createRouter({
     history: createWebHashHistory(),
-    routes
-})
+    routes,
+});
 
 export default router;
-export {
-    games
-};
+export { games };
